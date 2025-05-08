1d4fb82bae568b1f7477e94746d9665ccea3bd0d
Mauricio Quevedo <106039948+MauriNashe@users.noreply.github.com>
2025-05-07 22:54:33 -0300

    Test de conexiones

--- src/test/TestPrueba.java
+++ src/test/TestPrueba.java
@@ -1,11 +1,23 @@
 package test;
 
 import org.junit.Test;
-import algoritmo.UnionFind;
 import static org.junit.Assert.*;
+import modelo.Ciudad;
+import modelo.Mapa;
+import algoritmo.UnionFind;
+
 
 public class TestPrueba {
-    @Test
+	@Test
+	public void testCiudad() {
+		Mapa grafo = new Mapa();
+		Ciudad buenosAires = new Ciudad(0, "Buenos Aires");
+		Ciudad cordoba = new Ciudad(1, "Córdoba");
+		grafo.agregarCiudad(buenosAires);
+		grafo.agregarCiudad(cordoba);
+		grafo.agregarConexion(buenosAires, cordoba, 10);
+	}
+	@Test
     public void testFind() {
         UnionFind uf = new UnionFind(5);
         assertEquals(0, uf.find(0));
