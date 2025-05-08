6a2887d2ab57d53d4d36335257d8af588572b16a
Mauricio Quevedo <106039948+MauriNashe@users.noreply.github.com>
2025-05-07 22:44:53 -0300

    Prieros test del alg de Krustal

--- src/test/TestPrueba.java
+++ src/test/TestPrueba.java
@@ -1,5 +1,28 @@
 package test;
 
-public class TestPrueba {
+import org.junit.Test;
+import algoritmo.UnionFind;
+import static org.junit.Assert.*;
 
+public class TestPrueba {
+    @Test
+    public void testFind() {
+        UnionFind uf = new UnionFind(5);
+        assertEquals(0, uf.find(0));
+        assertEquals(3, uf.find(3));
+    }
+
+    @Test
+    public void testUnion() {
+        UnionFind uf = new UnionFind(3);
+        uf.union(0, 1);
+        assertTrue(uf.conectados(0, 1));
+        assertFalse(uf.conectados(0, 2));
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testFindConIndiceInvalido() {
+        UnionFind uf = new UnionFind(2);
+        uf.find(5);
+    }
 }
