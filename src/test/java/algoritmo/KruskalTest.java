fbf3f0f8cc6a662b34daaea6144ac9e72445fef4
Mauricio Quevedo <106039948+MauriNashe@users.noreply.github.com>
2025-05-12 22:56:08 -0300

    Test Krustal

--- src/test/java/algoritmo/KruskalTest.java
+++ src/test/java/algoritmo/KruskalTest.java
@@ -4,58 +4,48 @@ import main.java.modelo.Parque;
 import main.java.modelo.Estacion;
 import main.java.modelo.Sendero;
 import main.java.algoritmo.Kruskal;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class KruskalTest {
 
-    private Parque parque;
-    private Estacion estacion1, estacion2, estacion3, estacion4;
+	@Test
+    public void testAGM() {
+        Parque parque = new Parque("Reserva", -34.5, -58.4, 12);
 
-    @BeforeEach
-    void inicializarVariables() {
-		parque = new Parque("Parque Nacional", -34.1, -58.4, 10);
+        Estacion e0 = new Estacion(0, "Estacion A", -14.59, -54.15);
+        Estacion e1 = new Estacion(1, "Estacion B", -30.72, -15.69);
+        Estacion e2 = new Estacion(2, "Estacion C", -72.13, -82.36);
+        Estacion e3 = new Estacion(3, "Estacion D", -36.41, -38.42);
 
-		estacion1 = new Estacion(0, "Estacion A", -54.51, -48.11);
-		estacion2 = new Estacion(1, "Estacion B", -74.12, -38.92);
-		estacion3 = new Estacion(2, "Estacion C", -4.83, -84.32);
-        estacion4 = new Estacion(3, "Estacion D", -14.93, -78.44);
+        parque.agregarEstacion(e0);
+        parque.agregarEstacion(e1);
+        parque.agregarEstacion(e2);
+        parque.agregarEstacion(e3);
 
-        parque.agregarEstacion(estacion1);
-        parque.agregarEstacion(estacion2);
-        parque.agregarEstacion(estacion3);
-        parque.agregarEstacion(estacion4);
+        parque.agregarSendero(e0, e1, 10);
+        parque.agregarSendero(e1, e2, 5);
+        parque.agregarSendero(e2, e3, 3);
+        parque.agregarSendero(e0, e3, 7);
+        parque.agregarSendero(e1, e3, 8);
 
-        parque.agregarSendero(estacion1, estacion2, 4);
-        parque.agregarSendero(estacion1, estacion3, 3);
-        parque.agregarSendero(estacion2, estacion3, 1);
-        parque.agregarSendero(estacion2, estacion4, 2);
-        parque.agregarSendero(estacion3, estacion4, 5);
-    }
-
-    @Test
-    void testObtenerAGM() {
-        List<Sendero> agm = Kruskal.obtenerAGM(parque);
+        Kruskal kruskal = new Kruskal();
+        List<Sendero> agm = kruskal.obtenerAGM(parque);
         assertEquals(3, agm.size());
         int impactoTotal = agm.stream().mapToInt(Sendero::obtenerImpactoAmbiental).sum();
-        assertEquals(6, impactoTotal);
+        assertEquals(15, impactoTotal); 
     }
 
     @Test
-    void testAGMVacioSiNoHayEstaciones() {
-        Parque parqueVacio = new Parque("", 0.0, 0.0, 0);
-        List<Sendero> agm = Kruskal.obtenerAGM(parqueVacio);
+    public void testAGMVacio() {
+        Parque parque = new Parque("",0,0,0);
+
+        Kruskal kruskal = new Kruskal();
+        List<Sendero> agm = kruskal.obtenerAGM(parque);
+
+        assertNotNull(agm);
         assertTrue(agm.isEmpty());
-    }
-
-    @Test
-    void testTiempoDeEjecucion() {
-        Kruskal.obtenerAGM(parque);
-        double tiempo = new Kruskal().obtenerTiempoEjecucion();
-        assertTrue(tiempo > 0);
     }
 }
